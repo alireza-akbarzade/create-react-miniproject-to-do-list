ccb4cd11b9bd41f4c97e4b0ff06b653ea1117795
alireza <alireza.akbarzade88@gmail.com>
2026-02-14 18:55:56 +0330

    create submitHandler= add work

--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import TaskContext from './TaskContext';
 
-const TopForm = ()=>{
-    return(
+const TopForm = () => {
+
+    const [task, setTask] = useState("")
+
+
+    const { taskItem, setTaskItem } = useContext(TaskContext)
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        setTaskItem([...taskItem, { id: Math.random(), title: task, done: false }])
+        setTask("")
+
+
+
+    }
+
+    return (
         <>
             <h4 className="text-center text-info text_shdow">به to do خوش اومدید</h4>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className="form-group d-flex">
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={task} onChange={(event) => setTask(event.target.value)} />
                     <button type="submit" className="btn btn-success me-1">ثبت</button>
                 </div>
             </form>
