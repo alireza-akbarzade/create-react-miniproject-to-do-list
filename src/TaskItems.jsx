f164261e25c27e767212c8c5360bb17ab3114d19
alireza <alireza.akbarzade88@gmail.com>
2026-02-14 18:20:23 +0330

    change icon & delete  width onClick

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -4,6 +4,23 @@ import TaskContext from './TaskContext';
 
 const TaskItems = () => {
     const { taskItem, setTaskItem } = useContext(TaskContext)
+
+    const changeIcon = (id) => {
+        let newtask = [...taskItem]
+        newtask.forEach(task => {
+            if (task.id === id) {
+                task.done = !task.done
+            }
+            setTaskItem(newtask)
+        })
+    }
+    const DeleteHandler = (id) => {
+        let newtask = taskItem.filter(task=>task.id !==id)
+            setTaskItem(newtask)
+
+
+    }
+
     return (
         <ul className="list-group m-0 p-0 mt-2">
             {taskItem.map((item) => (
@@ -11,13 +28,15 @@ const TaskItems = () => {
                     {item.title}
                     <span>
                         {item.done ? (
-                            <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
+                            <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow" onClick={() => changeIcon(item.id)}></i>
                         ) : (
-                            <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
+                            <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow" onClick={() => changeIcon(item.id)}></i>
                         )}
 
 
-                        <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+                        <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow" onClick={() => {
+                            DeleteHandler(item.id)
+                        }}></i>
                     </span>
                 </li>
             ))}
